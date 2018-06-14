7cffc11d2ff0bb2a0a9f839116d33bb513d4a746
s-matyukevich <s.matyukevich@gmail.com>
2018-06-14 14:43:48 -0600

    external-ip custom

--- ci/tasks/deploy-bosh.sh
+++ ci/tasks/deploy-bosh.sh
@@ -13,7 +13,7 @@ if ! bosh create-env bosh-deployment/bosh.yml \
     --state=state.json \
     --vars-store=creds.yml \
     -o bosh-deployment/vsphere/cpi.yml \
-    -o bosh-deployment/external-ip-not-recommended.yml \
+    -o concourse-bosh-deployment/ci/opfiles/external-ip.yml \
     -v director_name=$director_name \
     -v internal_cidr=$BOSH_DC1_CIDR \
     -v internal_gw=$BOSH_DC1_INTERNAL_GW \
