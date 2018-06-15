54f4d3615b753a3b41998882600a7529461e1a47
s-matyukevich <s.matyukevich@gmail.com>
2018-06-15 12:43:34 -0600

    args escaped

--- ci/tasks/update-cloudconfig.sh
+++ ci/tasks/update-cloudconfig.sh
@@ -19,13 +19,13 @@ bosh interpolate concourse-bosh-deployment/ci/templates/cloud-config.yml \
   -v vcenter_dc2_cluster1=$BOSH_DC2_CIDR \
   -v bosh_dc1_cidr=$BOSH_DC1_CIDR \
   -v bosh_dc1_internal_gw=$BOSH_DC1_INTERNAL_GW \
-  -v bosh_dc1_dns=$BOSH_DC1_DNS \
-  -v bosh_dc1_network_reserved=$BOSH_DC1_NETWORK_RESERVED \
+  -v bosh_dc1_dns="$BOSH_DC1_DNS" \
+  -v bosh_dc1_network_reserved="$BOSH_DC1_NETWORK_RESERVED" \
   -v bosh_dc1_vcenter_network_name=$BOSH_DC1_VCENTER_NETWORK_NAME \
   -v bosh_dc2_cidr=$BOSH_DC2_CIDR \
   -v bosh_dc2_internal_gw=$BOSH_DC2_INTERNAL_GW \
   -v bosh_dc2_dns=$BOSH_DC2_DNS \
-  -v bosh_dc2_network_reserved=$BOSH_DC2_NETWORK_RESERVED \
+  -v bosh_dc2_network_reserved="$BOSH_DC2_NETWORK_RESERVED" \
   -v bosh_dc1_vcenter_network_name=$BOSH_DC1_VCENTER_NETWORK_NAME > cc.yml
 
 director_name=bosh-$CONCOURSE_TEAM
